37c1c35eed1ae0e8d46b4a73e6035a074187a058
neworderofjamie <neworderofjamie@gmail.com>
2025-04-23 18:29:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ml_genn_netx",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
 
     python_requires=">=3.7.0",
